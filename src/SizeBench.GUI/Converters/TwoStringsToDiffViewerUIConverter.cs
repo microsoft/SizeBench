5280c4a165cb9cdca1607150267e12957ea44b37
Austin Lamb <austin.lamb@microsoft.com>
2026-07-13 17:27:12 -0700

    Change from a simple FlowDocument to a custom UserControl that has two elements in it - a gutter with line numbers as well as the FlowDocument with the disassembly text lines.
    This way we don't lose the line numbers that DiffPlex.Wpf had, and they also still remain out of the text when copying.

--- src/SizeBench.GUI/Converters/TwoStringsToDiffViewerUIConverter.cs
+++ src/SizeBench.GUI/Converters/TwoStringsToDiffViewerUIConverter.cs
@@ -1,11 +1,7 @@
 ﻿using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Media;
-using DiffPlex.DiffBuilder;
-using DiffPlex.DiffBuilder.Model;
+using SizeBench.GUI.Controls;
 
 #nullable disable // Disabling nullability for IValueConverters, since WPF doesn't define them correctly yet (they should return object? not object)
 
@@ -13,17 +9,6 @@ namespace SizeBench.GUI.Converters;
 
 public sealed class TwoStringsToDiffViewerUIConverter : IMultiValueConverter
 {
-    private static readonly Brush InsertedBackground = new SolidColorBrush(Color.FromRgb(255, 255, 187));
-    private static readonly Brush DeletedBackground = new SolidColorBrush(Color.FromRgb(255, 168, 168));
-    private static readonly Brush ImaginaryBackground = new SolidColorBrush(Color.FromRgb(230, 230, 230));
-
-    static TwoStringsToDiffViewerUIConverter()
-    {
-        InsertedBackground.Freeze();
-        DeletedBackground.Freeze();
-        ImaginaryBackground.Freeze();
-    }
-
     public static TwoStringsToDiffViewerUIConverter Instance { get; } = new TwoStringsToDiffViewerUIConverter();
 
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
@@ -55,51 +40,7 @@ public sealed class TwoStringsToDiffViewerUIConverter : IMultiValueConverter
         }
 #pragma warning restore CA1508
 
-        var diff = InlineDiffBuilder.Diff(leftString, rightString);
-
-        var document = new FlowDocument
-        {
-            FontFamily = new FontFamily("Consolas"),
-            FontSize = 16,
-            PagePadding = new Thickness(0),
-        };
-
-        foreach (var line in diff.Lines)
-        {
-            var prefix = line.Type switch
-            {
-                ChangeType.Inserted => "+ ",
-                ChangeType.Deleted => "- ",
-                ChangeType.Modified => "~ ",
-                ChangeType.Imaginary => "  ",
-                _ => "  "
-            };
-
-            var background = line.Type switch
-            {
-                ChangeType.Inserted => InsertedBackground,
-                ChangeType.Deleted => DeletedBackground,
-                ChangeType.Imaginary => ImaginaryBackground,
-                _ => Brushes.Transparent
-            };
-
-            var paragraph = new Paragraph(new Run(prefix + (line.Text ?? string.Empty)))
-            {
-                Background = background,
-                Margin = new Thickness(0),
-                Padding = new Thickness(2, 0, 2, 0),
-            };
-
-            document.Blocks.Add(paragraph);
-        }
-
-        return new FlowDocumentScrollViewer
-        {
-            Document = document,
-            VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
-            HorizontalScrollBarVisibility = ScrollBarVisibility.Auto,
-            IsToolBarVisible = false,
-        };
+        return new SelectableDiffViewer(leftString, rightString);
     }
 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
